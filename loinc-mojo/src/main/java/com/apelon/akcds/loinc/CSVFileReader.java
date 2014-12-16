adade468c0fe0eded59ae108cb089b358c4ffe6b
darmbrust <daniel.armbrust.list@gmail.com>
2014-12-16 07:10:53 +0000

    Add missing copyright headers

--- loinc-mojo/src/main/java/com/apelon/akcds/loinc/CSVFileReader.java
+++ loinc-mojo/src/main/java/com/apelon/akcds/loinc/CSVFileReader.java
@@ -1,3 +1,21 @@
+/**
+ * Copyright Notice
+ *
+ * This is a work of the U.S. Government and is not subject to copyright
+ * protection in the United States. Foreign copyrights may apply.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.apelon.akcds.loinc;
 
 import gov.va.oia.terminology.converters.sharedUtils.ConsoleUtil;
@@ -11,6 +29,15 @@ import java.util.Arrays;
 import org.apache.commons.io.input.BOMInputStream;
 import au.com.bytecode.opencsv.CSVReader;
 
+/**
+ * 
+ * {@link CSVFileReader}
+ *
+ * Reads the CSV formatted release files of LOINC, and the custom release notes file
+ * to extract the date and time information.
+ * 
+ * @author <a href="mailto:daniel.armbrust.list@gmail.com">Dan Armbrust</a>
+ */
 public class CSVFileReader extends LOINCReader
 {
 	String[] header;
