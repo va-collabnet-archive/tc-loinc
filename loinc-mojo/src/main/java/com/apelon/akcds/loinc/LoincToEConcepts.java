1eae6f9a98c8db9551b7582309534ecc46bc2f9e
darmbrust <daniel.armbrust.list@gmail.com>
2015-01-05 08:25:40 +0000

    cleanup handling of the DebugMap file, add a disable-publish option.

--- loinc-mojo/src/main/java/com/apelon/akcds/loinc/LoincToEConcepts.java
+++ loinc-mojo/src/main/java/com/apelon/akcds/loinc/LoincToEConcepts.java
@@ -451,7 +451,7 @@ public class LoincToEConcepts extends ConverterBaseMojo
 			
 			// this could be removed from final release. Just added to help debug editor problems.
 			ConsoleUtil.println("Dumping UUID Debug File");
-			ConverterUUID.dump(new File(outputDirectory, "loincUuidDebugMap.txt"));
+			ConverterUUID.dump(outputDirectory, "loincUuid");
 			ConsoleUtil.println("LOINC Processing Completes " + new Date().toString());
 			ConsoleUtil.writeOutputToFile(new File(outputDirectory, "ConsoleOutput.txt").toPath());
 		}
